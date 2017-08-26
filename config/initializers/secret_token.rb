74ba727828faea104e602501d6b3bc0be2f7f8b4
ashag <asha@kickstarter.com>
2017-08-26 17:16:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirdFridays::Application.config.secret_key_base = 'e68f8378dc0aa8c0cc56e9bd68aeb1acc5de390865e24e1699c2ba0ba31a025f0df7700d8e5aa67008bdc3670e4f33868f9c50841bac9267c1b043eba7052f0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThirdFridays::Application.config.session_store :cookie_store, key: '_third_fridays_session'
